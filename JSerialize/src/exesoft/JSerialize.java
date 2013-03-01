4e93684b1fe954774632a55cc8f7b7a4400998c9
Karol Kokoszka <karol.k91@gmail.com>
2013-03-01 10:05:37 +0100

    Początkowe tworzenie interfejsu klasy JSerialize

--- JSerialize/src/exesoft/JSerialize.java
+++ JSerialize/src/exesoft/JSerialize.java
@@ -2,9 +2,16 @@ package exesoft;
 
 public class JSerialize {
 
-	/**
-	 * @param args
-	 */
+	public String read(){
+		String json = new String();
+		
+		return json;
+	}
+	
+	public void write(){
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
