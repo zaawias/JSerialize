fa44d41a048628ccc11fe7ae25c70ea00180654c
Karol Kokoszka <karol.k91@gmail.com>
2013-03-01 11:33:31 +0100

    Poczatkowy interfejs

---
+++ src/exesoft/JSerialize.java
@@ -0,0 +1,31 @@
+package exesoft;
+
+import java.io.InputStream;
+
+public class JSerialize {
+
+	
+	public String read(){
+		String json = new String();
+		
+		return json;
+	}
+	
+	public void write(){
+		
+	}
+	
+	public JSerialize(){
+		
+	}
+	
+	public JSerialize(InputStream in){
+		
+	}
+	
+	public static void main(String[] args) {
+
+
+	}
+
+}
